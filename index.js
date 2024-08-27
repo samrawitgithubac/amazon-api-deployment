764fcc41332154a7d3217bb65447bef1ded90748
samrawitgithubac <149234089+samrawitgithubac@users.noreply.github.com>
2024-08-26 22:42:42 -0700

    first commit

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { setGlobalOptions } = require("firebase-functions/v2");
 dotenv.config();
-
 const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 const app = express();
-
-setGlobalOptions({ maxInstances: 10 });
-
 app.use(cors({ origin: true }));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "success !" });
+  res.status(200).json({
+    message: "Success !",
+  });
 });
 
-app.post("/payment/create", async (req, res) => {
-  const total = parseInt(req.query.total);
+app.post("/payments/create", async (req, res) => {
+  const total = req.query.total;
+
   if (total > 0) {
     const paymentIntent = await stripe.paymentIntents.create({
       amount: total,
-      currency: "USD",
+      currency: "usd",
     });
+
     res.status(201).json({
       clientSecret: paymentIntent.client_secret,
     });
   } else {
-    res.status(403).json({ message: "total must be grater than 0" });
+    res.status(403).json({ message: "total must be greater than 0" });
   }
 });
+// app listen by using firebase
 app.listen(5000, (err) => {
   if (err) throw err;
   console.log("Amazon Server Running on PORT:5000,http://localhost:5000");
 });
-//exports.api = onRequest(app);
